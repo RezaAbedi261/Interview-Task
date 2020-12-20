d92a2067df2bf45476b4378d690007e7d6ee8580
Reza <rabedi261@gmail.com>
2020-12-20 20:08:02 +0330

    make PlaceInfo and Comment Parcelable

--- app/src/main/java/com/task/interview/model/Comment.kt
+++ app/src/main/java/com/task/interview/model/Comment.kt
@@ -1,10 +1,13 @@
 package com.task.interview.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Comment(
     val name: String,
     val text: String,
     val date: String,
     val photo: String,
     val rate: String
-)
+) : Parcelable
