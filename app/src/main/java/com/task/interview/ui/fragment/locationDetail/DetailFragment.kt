bf2d5fa865d0b8cef6832118d4992d6cc00532f6
Reza <rabedi261@gmail.com>
2020-12-21 12:37:31 +0330

    add view of DetailFragment

--- app/src/main/java/com/task/interview/ui/fragment/locationDetail/DetailFragment.kt
+++ app/src/main/java/com/task/interview/ui/fragment/locationDetail/DetailFragment.kt
@@ -3,20 +3,28 @@ package com.task.interview.ui.fragment.locationDetail
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.TransitionInflater
 import com.bumptech.glide.Glide
 import com.task.interview.R
 import com.task.interview.base.BaseFragment
 import com.task.interview.databinding.FragmentDetailBinding
+import com.task.interview.ui.customview.CommentView
 import kotlinx.android.synthetic.main.fragment_detail.*
+import java.lang.StringBuilder
 
 
 class DetailFragment : BaseFragment<FragmentDetailBinding, DetailFragmentVM>() {
 
     override val viewModel: DetailFragmentVM by viewModels()
     val args: DetailFragmentArgs by navArgs()
+    private lateinit var recyclerViewAdapter: ImagesRecyclerViewAdapter
 
     override fun layout() = R.layout.fragment_detail;
 
@@ -36,7 +44,26 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailFragmentVM>() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             imageView.transitionName = "imageView"
         }
-        Glide.with(this).load(args.placeInfo.background_photo).into(imageView)
+        val locationInfo = args.placeInfo
+        Glide.with(this).load(locationInfo.background_photo).into(imageView)
+
+        tvName.text = "${locationInfo.name}"
+        tvType.text = locationInfo.type
+        tvTime.text = "ساعات کاری : ${locationInfo.open}  ${locationInfo.close}"
+        tvAddress.text = "آدرس : ${locationInfo.address}"
+        var options = StringBuilder()
+        locationInfo.options.forEach {
+           options.append(it+"\n")
+        }
+        tvOptions.text = "ویژگی‌ها :\n$options"
+        imagesRecyclerView
+
+        recyclerViewAdapter = ImagesRecyclerViewAdapter(locationInfo.photos)
+        imagesRecyclerView.adapter= recyclerViewAdapter
+
+        locationInfo.comments.forEach {
+            llComments.addView(CommentView(requireContext()).setData(it))
+        }
     }
 
 
